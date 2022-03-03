6509aa3ba9540ef166e8e680c6a18211b80a5f54
lachlan knell <lachlanleoknell@gmail.com>
2022-03-03 10:35:54 +1000

    add

--- mathAssignment.c
+++ mathAssignment.c
@@ -20,12 +20,12 @@
 
 int main()
 {
-	int num, i, d1, d2, d3, pwin=0, bet, money=0, pwin3fk=0, pwin3row=0, pwin18=0, pwin20=0, pwin24=0;
-	double per=0.0, cas=100.0;
+	int num, i, d1, d2, d3, pwin=0, money=0, pwin3fk=0, pwin3row=0, pwin18=0, pwin20=0, pwin24=0;
+	double per=0.0, cas=100.0, hedge;
 	time_t t1;
 	
 
-	printf("How many times will you be simulating this specific dice rolling equation?\n");
+	printf("How many times will you be simulating this specific dice rolling simulation?\n");
 	scanf(" %d", &num);
 
 	srand((unsigned) time (&t1));
@@ -43,73 +43,73 @@ int main()
 		d1++;d2++;d3++;
 		int dt=d1+d2+d3;
 		/* compare dice */
-		
-		
-		if (dt >= 20 && dt < 24)
+		if (dt == 24)
 		{
 			pwin++;
-			pwin20++;
-			money += 2576.749;
+			pwin24++;
+			money += 15450;
 		}
+			
 	
 		if (d1 == d2 && d1 == d3)
 		{
 			pwin++;
 			pwin3fk++;
-			money += 3000;
+			money += 6200; 
 		}
-			
-		if (dt == 24)
+		
+		if (dt >=22 && dt < 24)
 		{
 			pwin++;
-			pwin24++;
-			money += 10000;
+			pwin20++;
+			money += 5850;
 		}
-		 
-		if ((d1 + d2 + d3) >= 18 && (d1+d2+d3) < 20)
+	 
+		if (dt >= 20 && dt < 22)
 		{
 			pwin++;
 			pwin18++;
-			money += 2200;
+			money += 4500;
 		}
 			
 		if ((d1+1 == d2 && d1+2 == d3) || (d1+1 == d3 && d1+2 == d2) || (d2+1 == d3 && d2+2 == d1) || (d2+1 == d1 && d2+2 == d3) || (d3+1 == d2 && d3+2 == d1) || (d3+1 == d1 && d3+2 ==d2))
 		{
 			pwin++;
 			pwin3row++;
-			money += 2250;
+			money += 3500;
 		}
 		
 	}
 		
-	printf("%d\n", pwin);
-	printf("%d\n\n", money);
+	printf("Player Wins-%d\n", pwin);
+	printf("Money Made-%d\n\n", money);
 	printf("%d-3 of a kind", pwin3fk);
+
 	per=((double)pwin3fk/(double)num)*100;
 	printf("\n%f\%\n\n", per);
 	cas=cas-per;
 	
-	printf("%d- 3 in a row", pwin3row);
+	printf("%d -3 in a row", pwin3row);
 	per=((double)pwin3row/(double)num)*100;
 	printf("\n%f\%\n\n", per);
 	cas=cas-per;
 	
-	printf("%d- 18", pwin18);
+	printf("%d -20", pwin18);
 	per=((double)pwin18/(double)num)*100;
 	printf("\n%f\%\n\n", per);
 	cas=cas-per;
 	
-	printf("%d- 20", pwin20);
+	printf("%d -22", pwin20);
 	per=((double)pwin20/(double)num)*100;
 	printf("\n%f\%\n\n", per);
 	cas=cas-per;
 	
-	printf("%d- 24", pwin24);
+	printf("%d -24", pwin24);
 	per=((double)pwin24/(double)num)*100;
 	printf("\n%f\%\n\n", per);
 	cas=cas-per;
 	
-	
+	printf("%f\n", hedge);
 	printf("%f", cas);
 	return 0;
 }
